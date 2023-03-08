76dfda18ea9941186b34007a952701f2e2294a8c
Natasha <natkamonte1992@gmail.com>
2023-03-08 15:50:13 +0200

    .

--- src/components/Loader/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import { Ring, LoaderSpan } from "./Loader.styled";
-
-export const Loader = () => {
-    return (
-        <Ring className="ring">Loading
-        <LoaderSpan></LoaderSpan>
-      </Ring>
-    );
-  };
